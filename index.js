3ae8af54014278108252ce0cdd07d5986a984c51
Adam <adam.pol1234@gmail.com>
2017-10-19 20:21:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import sensor from 'node-dht-sensor';
+import fetch from 'node-fetch';
+
+const GPIO_PORT = process.env.GPIO_PORT || 21;
+const DHT_SENSOR = process.env.DHT_SENSOR || 11;
+const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3000/api/params';
+
+setInterval(() => {
+    sensor.read(DHT_SENSOR, GPIO_PORT, (err, temperature, humidity) => {
+        if (!err) {
+            console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+                'humidity: ' + humidity.toFixed(1) + '%'
+            );
+
+            fetch(SERVER_URL, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: {temperature, humidity}
+            })
+            .then(() => console.log('send success'))
+            .catch(err => console.log(err))
+        }
+
+        else {
+            console.log(err);
+        }
+    })
+}, 1000);
