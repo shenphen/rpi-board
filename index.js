1a1067c3793a4c660ca92a746fa1c64eeff1432a
Adam <adam.pol1234@gmail.com>
2017-10-21 13:32:35 +0200

    feat(js): refreactor & add setSignals method

--- index.js
+++ index.js
@@ -4,83 +4,123 @@ const five = require('johnny-five');
 const Raspi = require('raspi-io');
 const moment = require('moment');
 
-const GPIO_PORT = process.env.GPIO_PORT || 21;
 const DHT_SENSOR = process.env.DHT_SENSOR || 11;
+
+const DHT_GPIO = process.env.DHT_GPIO || 21;
+const HEATER_GPIO = process.env.HEATER_GPIO || 17;
+const COOLER_GPIO = process.env.COOLER_GPIO || 22;
+const HUMIDITIFIER_GPIO = process.env.HUMIDITIFIER_GPIO || 27;
+
 const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3000/api/params';
 
-const board = new five.Board({
-    io: new Raspi()
-});
+class Board {
+    constructor() {
 
-board.on('ready', function() {
-    var led = new five.Led('P1-13');
-    led.blink();
-});
+        this.board = new five.Board({
+            io: new Raspi()
+        });
 
-function onReady() {
+        this.HEATER = new five.Led(`GPIO${HEATER_GPIO}`);
+        this.COOLER = new five.Led(`GPIO${COOLER_GPIO}`);
+        this.HUMIDITIFIER = new five.Led(`GPIO${HUMIDITIFIER_GPIO}`);
 
-    setInterval(() => {
+        this.state = {
+            cooler: false,
+            heater: false,
+            humiditifier: false
+        }
 
-        const data = getData();
-        if(data) {
-            const { temperature, humidity, time } = data;
-            const daytime = moment.unix(time) < 19 && moment.unix(time) > 9;
+        this.board.on('ready', this.onReady.bind(this));
+        this.board.on('exit', this.onExit);
 
-            let HEATER = null;
-            let COOLER = null;
+        this.setSignals.bind(this);
+    }
 
-            const HUMIDITIFIER = humidity < 60;
-
-            if(daytime) {
-                HEATER = temperature < 24;
-                COOLER = temperature > 27;
+    onReady() {
+       
+        setInterval(() => {
+    
+            const data = this.getData();
+            if(data) {
+                setSignals(data);
+                postDataToServer({temperature, humidity, time});
             }
+    
+        }, 1000);
+    }
+
+    getData() {
+        sensor.read(DHT_SENSOR, DHT_GPIO, (err, temperature, humidity) => {
+            if (!err) {
+    
+                const time = `${Date.now()}`.slice(0, -3);
+    
+                console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+                    'humidity: ' + humidity.toFixed(1) + '%'
+                );
+    
+                return {temperature, humidity, time};
+    
+            }
+    
             else {
-                HEATER = temperature < 16;
-                COOLER = temperature > 18;
+                console.log(err);
+                return null;
             }
+        })
+    }
 
-            console.log({HUMIDITIFIER, HEATER, COOLER});
+    setSignals(data) {
+        const { temperature, humidity, time } = data;
+        const daytime = moment.unix(time) < 19 && moment.unix(time) > 9;
+        const { heater: prevHeaterState,
+                cooler: prevCollerState,
+                humiditifier: prevHumiditifierState } = this.state;
+        
+        let heaterState = null;
+        let coolerState = null;
+        const humiditifierState = humidity < 60;
 
-            postDataToServer({temperature, humidity, time});
+
+        if(daytime) {
+            heaterState = temperature < 24;
+            coolerState = temperature > 27;
         }
-
-
-    }, 1000);
-}
-
-function getData() {
-    sensor.read(DHT_SENSOR, GPIO_PORT, (err, temperature, humidity) => {
-        if (!err) {
-
-            const time = `${Date.now()}`.slice(0, -3);
-
-            console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
-                'humidity: ' + humidity.toFixed(1) + '%'
-            );
-
-            return {temperature, humidity, time};
-
-        }
-
         else {
-            console.log(err);
-            return null;
+            heaterState = temperature < 16;
+            coolerState = temperature > 18;
         }
-    })
+
+        heaterState && !prevHeaterState ? this.HEATER.on() : this.HEATER.off();
+        coolerState && !prevCoolerState ? this.COOLER.on() : this.COOLER.off();
+        humiditifierState && !prevHumiditifierState ? this.HUMIDITIFIER.on() : this.HUMIDITIFIER.off();
+
+        this.state = {
+            cooler: coolerState,
+            heater: heaterState,
+            humiditifier: humiditifierState
+        }
+
+        console.log({HUMIDITIFIER, heaterState, coolerState});
+    }
+
+    postDataToServer(data) {
+    
+        fetch(SERVER_URL, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        })
+        .then(() => console.log('send success'))
+        .catch(err => console.log(err))
+    }
+
+    onExit() {
+        this.HEATER.off();
+        this.COOLER.off();
+        this.HUMIDITIFIER.off();
+        console.log(`About to exit with code: ${code}`);
+    }
 }
 
-function postDataToServer(data) {
-
-    fetch(SERVER_URL, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
-    })
-    .then(() => console.log('send success'))
-    .catch(err => console.log(err))
-}
-
-process.on('exit', (code) => {
-    console.log(`About to exit with code: ${code}`);
-});
+const board = new Board();
