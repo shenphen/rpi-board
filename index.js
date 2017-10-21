5b653af6e1b8a26b2728c8f4c26f0f6dba077fc7
Adam <adam.pol1234@gmail.com>
2017-10-21 12:33:19 +0200

    feat(js): split code on functions & add johhny five test function

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 const sensor = require('node-dht-sensor');
 const fetch = require('node-fetch');
+const five = require('johnny-five');
+const Raspi = require('raspi-io');
+const moment = require('moment');
 
 const GPIO_PORT = process.env.GPIO_PORT || 21;
 const DHT_SENSOR = process.env.DHT_SENSOR || 11;
 const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3000/api/params';
 
-setInterval(() => {
+const board = new five.Board({
+    io: new Raspi()
+});
+
+board.on('ready', function() {
+    var led = new five.Led('P1-13');
+    led.blink();
+});
+
+function onReady() {
+
+    setInterval(() => {
+
+        const data = getData();
+        if(data) {
+            const { temperature, humidity, time } = data;
+            const daytime = moment.unix(time) < 19 && moment.unix(time) > 9;
+
+            let HEATER = null;
+            let COOLER = null;
+
+            const HUMIDITIFIER = humidity < 60;
+
+            if(daytime) {
+                HEATER = temperature < 24;
+                COOLER = temperature > 27;
+            }
+            else {
+                HEATER = temperature < 16;
+                COOLER = temperature > 18;
+            }
+
+            console.log({HUMIDITIFIER, HEATER, COOLER});
+
+            postDataToServer({temperature, humidity, time});
+        }
+
+
+    }, 1000);
+}
+
+function getData() {
     sensor.read(DHT_SENSOR, GPIO_PORT, (err, temperature, humidity) => {
         if (!err) {
 
@@ -15,17 +59,28 @@ setInterval(() => {
                 'humidity: ' + humidity.toFixed(1) + '%'
             );
 
-            fetch(SERVER_URL, {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({temperature, humidity, time})
-            })
-            .then(() => console.log('send success'))
-            .catch(err => console.log(err))
+            return {temperature, humidity, time};
+
         }
 
         else {
             console.log(err);
+            return null;
         }
     })
-}, 1000);
+}
+
+function postDataToServer(data) {
+
+    fetch(SERVER_URL, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    })
+    .then(() => console.log('send success'))
+    .catch(err => console.log(err))
+}
+
+process.on('exit', (code) => {
+    console.log(`About to exit with code: ${code}`);
+});
