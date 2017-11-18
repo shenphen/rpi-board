ee23edb20fa6443a598bf2cc9106a5aca88e6871
Adam <adam.pol1234@gmail.com>
2017-11-18 11:16:15 +0100

    feat(js/socket): install socket io client & configure program to take manual control orders first

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fetch = require('node-fetch');
 const five = require('johnny-five');
 const Raspi = require('raspi-io');
 const moment = require('moment');
+const io = require('socket.io-client');
 
 const DHT_SENSOR = process.env.DHT_SENSOR || 11;
 
@@ -11,7 +12,8 @@ const HEATER_GPIO = process.env.HEATER_GPIO || 13;
 const COOLER_GPIO = process.env.COOLER_GPIO || 6;
 const HUMIDITIFIER_GPIO = process.env.HUMIDITIFIER_GPIO || 26;
 
-const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3000/api/params';
+const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3000';
+const API_PATH = process.env.API_PATH || '/api/params';
 
 class Board {
     constructor() {
@@ -23,16 +25,23 @@ class Board {
         this.state = {
             cooler: false,
             heater: false,
-            humiditifier: false
+            humiditifier: false,
+            manualControl: null,
         }
 
         this.board.on('ready', this.onReady.bind(this));
         this.board.on('exit', this.onExit.bind(this));
 
-        this.setSignals.bind(this);
+        this.setSignals = this.setSignals.bind(this);
+        this.onControl = this.onControl.bind(this);
     }
 
     onReady() {
+
+        this.socket = io(SERVER_URL);
+        this.socket.on('connect', () => console.log('connect'));
+        this.socket.on('control', this.onControl)
+        this.socket.on('disconnect', () => {this.setState({manualControl: null}); console.log('disconnect')});
 
         this.HEATER = new five.Led(`GPIO${HEATER_GPIO}`);
         this.COOLER = new five.Led(`GPIO${COOLER_GPIO}`);
@@ -79,20 +88,29 @@ class Board {
         const daytime = hour < 19 && hour > 9;
         const { heater: prevHeaterState,
                 cooler: prevCollerState,
-                humiditifier: prevHumiditifierState } = this.state;
+                humiditifier: prevHumiditifierState,
+                manualControl } = this.state;
         
         let heaterState = null;
         let coolerState = null;
-        const humiditifierState = humidity < 60;
 
-
-        if(daytime) {
-            heaterState = temperature < 24;
-            coolerState = temperature > 27;
+        if(manualControl) {
+            heaterState = manualControl.heater;
+            coolerState = manualControl.cooler;
+            humiditifierState = manualControl.humiditifier;
         }
         else {
-            heaterState = temperature < 16;
-            coolerState = temperature > 18;
+            
+            const humiditifierState = humidity < 60;
+    
+            if(daytime) {
+                heaterState = temperature < 24;
+                coolerState = temperature > 27;
+            }
+            else {
+                heaterState = temperature < 16;
+                coolerState = temperature > 18;
+            }
         }
 
         heaterState ? this.HEATER.on() : this.HEATER.off();
@@ -110,13 +128,31 @@ class Board {
 
     postDataToServer(data) {
     
-        fetch(SERVER_URL, {
+        fetch(SERVER_URL + API_PATH, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(data)
         })
         .then(() => console.log('send success'))
         .catch(err => console.log(err))
+    }
+
+    onControl(data) {
+        if(data.manualControl && data.state) {
+            const { cooler, heater, humiditifier } = data.state;
+            this.setState({
+                manualControl: {
+                    cooler,
+                    heater,
+                    humiditifier
+                }
+            });
+        }
+        else {
+            this.setState({
+                manualControl: null
+            })
+        }
     }
 
     onExit() {
